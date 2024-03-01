df23a5204cc9e2aa3f9583de15455fc29cb11956
rezapahlevi42 <143397168+rezapahlevi42@users.noreply.github.com>
2024-03-01 19:20:49 +0700

    Add files via upload

---
+++ t1225150400111006.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $Nama= "Reza Fikri Pahlevi";
+        $NIM= "225150400111006";
+        for ($i = 0; $i<5; $i++){
+            echo "Nama = $Nama &nbsp;&nbsp; &nbsp;&nbsp; NIM = $NIM <br>";
+        } 
+        ?>
+    </body>
+</html>
